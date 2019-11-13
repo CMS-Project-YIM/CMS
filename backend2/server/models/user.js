461f7671a0c36f8bab873156204f78763efedd24
Yongyut Srisuban <yongyut120@hotmail.co.th>
2019-11-13 14:10:11 +0700

    ทำการเพิ่มิเพิ่ม backend2 - close #45

---
+++ backend2/server/models/user.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  User.associate = (models) => {
+    // associations can be defined here
+    User.hasMany(models.Book, {
+      foreignKey: 'userId',
+    });
+  };
+  return User;
+};
