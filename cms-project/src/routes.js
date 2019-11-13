1b62f638c16ac4da20f71ab52aea59f11a046f24
Puwaned Yamwised <puwaneda@gmail.com>
2019-11-13 15:08:45 +0700

    add postform route

--- cms-project/src/routes.js
+++ cms-project/src/routes.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { Router, Route } from 'react-router';
-import App from './component/app';
-import Chartbar from './component/chart'
-import Chartline from './component/chart'
-import Chart from './component/chart'
-import Message from './component/message/index'
-import Navbar from './component/navbar/navbar'
-import Login from './component/login/login'
+import React from "react";
+import { Router, Route } from "react-router";
+import App from "./component/app";
+import Chartbar from "./component/chart";
+import Chartline from "./component/chart";
+import Chart from "./component/chart";
+import Message from "./component/message/index";
+import Navbar from "./component/navbar/navbar";
+import Login from "./component/login/login";
+import PostForm from "./component/PostForm";
 
-const Routes = (props) => (
-    <Router {...props}>
-        <Route path="/" component={App} />
-        <Route path="/chart" component={Chart} />
-        <Route path="/chartbar" component={Chartbar} />
-        <Route path="/chartline" component={Chartline} />
-        <Route path="/message" component={Message} />
-        <Route path="/navbar" component={Navbar} />
-        <Route path="/login" component={Login} />
-    </Router>
+const Routes = props => (
+  <Router {...props}>
+    <Route path="/" component={App} />
+    <Route path="/chart" component={Chart} />
+    <Route path="/chartbar" component={Chartbar} />
+    <Route path="/chartline" component={Chartline} />
+    <Route path="/message" component={Message} />
+    <Route path="/navbar" component={Navbar} />
+    <Route path="/login" component={Login} />
+    <Route path="/PostForm" component={PostForm} />
+  </Router>
 );
 
 export default Routes;
