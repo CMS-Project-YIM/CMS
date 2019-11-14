f14f136c70201021b53a3d111f343dcd91b4fd13
Yongyut Srisuban <yongyut120@hotmail.co.th>
2019-11-14 15:13:12 +0700

    close #48 à¸à¸- ทำการเพิ่ม Class Gender

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -18,22 +18,8 @@ const user = (sequelize, DataTypes) => {
   });
 
   User.associate = (models) => {
-    // User.hasMany(models.Message, { onDelete: 'CASCADE' });
+    User.belongsTo(models.Gender, { onDelete: 'CASCADE'});
     User.belongsTo(models.userTypes, { onDelete: 'CASCADE'});
-  };
-
-  User.findByLogin = async login => {
-    let user = await User.findOne({
-      where: { username: login },
-    });
-
-    if (!user) {
-      user = await User.findOne({
-        where: { email: login },
-      });
-    }
-
-    return user;
   };
 
   return User;
