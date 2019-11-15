0ea3e33e7f5a475b0380029f7fac01f4f54e781a
Wiraphong-boat <wiraphong.ch.work@gmail.com>
2019-11-15 11:09:05 +0700

    display gender - close#52

--- cms-project/src/component/chart/chartbar.js
+++ cms-project/src/component/chart/chartbar.js
@@ -1,36 +1,58 @@
 import React, { Component } from "react";
 import { Bar } from "react-chartjs-2";
 
-const bar = {
-  labels: [
-    //ชื่อ catagory
-    "catagory1",
-    "catagory2",
-    "catagory3",
-    "catagory4",
-    "catagory5",
-    "catagory6",
-    "catagory7"
-  ],
-  datasets: [
-    {
-      label: "The number of posts of each catagory",
-      backgroundColor: "#ff4d4f",
-      borderColor: "rgba(255,99,132,1)",
-      borderWidth: 1,
-      hoverBackgroundColor: "#ff4d4f",
-      hoverBorderColor: "rgba(255,99,132,1)",
-      data: [65, 59, 80, 81, 56, 55, 40, 0] //ข้อมูลแสดงผล
-    }
-  ]
-};
+const getNameCatagory = "http://localhost:9000/catagory/getCatagory";
 
 export class Chartbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameCatagory: []
+    };
+  }
+  componentDidMount() {
+    this.getCatagory();
+  }
+
+  //getNamecatalog
+  getCatagory = () => {
+    fetch(getNameCatagory)
+      .then(response => response.json(response))
+      .then(response => {
+        this.setState({
+          nameCatagory: [
+            response[0].catagorytype,
+            response[1].catagorytype,
+            response[2].catagorytype,
+            response[3].catagorytype
+          ]
+        });
+      });
+  };
+
+  showdata = () => {
+    const bar = {
+      labels: this.state.nameCatagory,
+      datasets: [
+        {
+          label: "The number of posts of each catagory",
+          backgroundColor: "#ff4d4f",
+          borderColor: "rgba(255,99,132,1)",
+          borderWidth: 1,
+          hoverBackgroundColor: "#ff4d4f",
+          hoverBorderColor: "rgba(255,99,132,1)",
+          data: [65, 59, 80, 81, 56, 55, 40, 0] //ข้อมูลแสดงผล
+        }
+      ]
+    };
+    return bar;
+  };
+
   render() {
     return (
       <div>
         <div className="chart-wrapper">
-          <Bar data={bar} />
+          <Bar data={this.showdata} />
         </div>
       </div>
     );
